04166d446db4aca627fac8ee66851cafecede6a8
Are Edvardsen <are.edvardsen@helse-nord.no>
2019-07-25 14:08:11 +0200

    data docs

---
+++ R/innbAldKj2014_2018.R
@@ -0,0 +1,25 @@
+#' Norsk befolkningsdata om demografi og tilhørighet i spesialisthelsesystemet
+#'
+#' Et datasett med norsk befolkningsdata fra og med 2014 til og med 2018 om
+#' demografi (alder, kjønn, (bydel), kommune, fylke) og tilhørighet i
+#' spesialisthelsetjenesten (helseforetak, regionalt helseforetak)
+#'
+#' @format Dataramme med 505 620 observasjoner og 14 variabler
+#' \describe{
+#'   \item{Aar}{}
+#'   \item{ErMann}{}
+#'   \item{Alder}{}
+#'   \item{KomNr}{}
+#'   \item{KomNrTxt}{}
+#'   \item{Fylke}{}
+#'   \item{FylkeNr}{}
+#'   \item{BoHF}{}
+#'   \item{BoHFnr}{}
+#'   \item{BoRHF}{}
+#'   \item{BoRHFnr}{}
+#'   \item{Bydel}{}
+#'   \item{BydelNr}{}
+#'   \item{Innbyggere}{}
+#' }
+#' @source \url{}
+"innbAldKj2014_2018"
