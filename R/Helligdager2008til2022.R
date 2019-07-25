4994557b39c268bec9226e746540be9a149bf331
Are Edvardsen <are.edvardsen@helse-nord.no>
2019-07-25 13:23:33 +0200

    correction

--- R/Helligdager2008til2022.R
+++ R/Helligdager2008til2022.R
@@ -3,9 +3,11 @@
 #' Et datasett med norske røddager 2008 til 2022. Søndager
 #' er ikke inkludert i listen.
 #'
-#' @format En data frame med 195 rader og 1 kolonne
+#' @format En data frame med 195 rader og 2 kolonner
 #' \describe{
 #'   \item{Dato}{Dato helligdagen faller på, i datoformat}
+#'   \item{Helligdag}{Hvilken helligdag det gjelder (inneholder foreløpig bare
+#'   tomme verdier)}
 #' }
 #' @source \url{http://www.norskkalender.no/}
 "Helligdager2008til2022"
